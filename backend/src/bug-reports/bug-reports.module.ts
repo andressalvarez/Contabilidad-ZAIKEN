5c1b41dfd2c8ddeab1536f5752f3990b969222ef
Andres Felipe Salamanca Alvarez <asalamanca@rootstack.com>
2026-02-10 04:57:03 -0500

    fix(backend): import CaslModule in BugReportsModule for PermissionsGuard DI

--- backend/src/bug-reports/bug-reports.module.ts
+++ backend/src/bug-reports/bug-reports.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SecurityModule } from '../security/security.module';
+import { CaslModule } from '../casl/casl.module';
 import { BugReportsController } from './bug-reports.controller';
 import { BugReportsService } from './bug-reports.service';
 
 @Module({
-  imports: [PrismaModule, SecurityModule],
+  imports: [PrismaModule, SecurityModule, CaslModule],
   controllers: [BugReportsController],
   providers: [BugReportsService],
   exports: [BugReportsService],
